ce3749c0ee3e02353d5e983d41253ee115182629
bolollo <bolosig@gmail.com>
2013-06-29 23:06:47 +0200

    archivos de las lectura 8

--- js/script_mapa.js
+++ js/script_mapa.js
@@ -2,12 +2,26 @@
 var map;
 
 function init() {
+    var options = {
+        projection: new OpenLayers.Projection("EPSG:4326"),
+        units: "degrees",
+        numZoomLevels: 10,
+        maxExtent: new OpenLayers.Bounds(-90, 0, 1, 45)
+    };
+
     //Creamos un nuevo objeto map en el div con el id = map
-    map = new OpenLayers.Map('map');
+    map = new OpenLayers.Map('map',options);
     //Creamos una capa de tipo WMS
     var ol_wms = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
     //Agregamos la capa creada al mapa
     map.addLayer(ol_wms);
+	
+	//agregar el control de leyenda al mapa
+	map.addControl(new OpenLayers.Control.LayerSwitcher());
+	
+	//agregar el control de mapa de referencia
+	map.addControl( new OpenLayers.Control.OverviewMap());
+	
     //Hacemos un zoom a la extensión máxima del mapa.
     map.zoomToMaxExtent();
 }
