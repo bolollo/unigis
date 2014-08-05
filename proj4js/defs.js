6b3d0bd209af936ce38ca3dcbe906a1179bb9dff
bolollo <bolosig@gmail.com>
2014-08-05 18:19:06 +0200

    mi primer programa y convertidor de srs

---
+++ proj4js/defs.js
@@ -0,0 +1,9 @@
+proj4.defs([
+  [
+    'EPSG:23031',
+    '+proj=utm +zone=31 +ellps=intl +towgs84=-136.65549,-141.46580,-167.29848,-2.09308759,-0.00140548839,-0.107708594,1.000011546110 +units=m +no_defs'],
+  [
+    'EPSG:23030',
+    '+proj=utm +zone=30 +ellps=intl +units=m +no_defs'
+  ]
+]);
